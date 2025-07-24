d5566934a8ada34f1acb38c50d83b49eda9025b4
meryem1204 <meryemarsln28@gmail.com>
2025-07-24 13:43:53 +0000

    update

--- explain.sh
+++ explain.sh
@@ -9,15 +9,15 @@ file=$1
 fi 
 echo "Verilen dizinin boyutu"
 du -sh  $file
-echo "Verilen dizindeki dosya sayısı"
-du -ah $file | sort -nr | head -1
 echo "Verilen dizindeki en yüksek boyutlu dosya ve boyutu"
+du -ah $file | sort -nr | head -1
+echo "Verilen dizindeki en düşük boyutlu dosya ve boyutu"
 du -ah $file | sort -r | head -1
-echo "Verilen dizindeki en düşük boyutlu dosya ve boyutu" 
-ls $file | wc -l
+echo "Verilen dizindeki dosya sayısı" 
+find $file -maxdepth 1 -type f | wc -l
 echo "Verilen dizindeki klasör sayısı"
 find $file -maxdepth 1 -type d | wc -l
 echo "Verilen dizinin sahibi"
-ls -l 
+stat -c '%U' $file 
 echo "Verilen dizindeki dosyaların uzantı listesi"
 find $file -type f | sed 's/^\.\///g' | sort
